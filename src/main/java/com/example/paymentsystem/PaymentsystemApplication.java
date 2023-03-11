bbaeca5c63f42792fb92678b8e78037b5f897983
Ivan.Milev <ivan.milev@flolive.net>
2023-03-11 07:54:36 +0200

    Initial commit

---
+++ src/main/java/com/example/paymentsystem/PaymentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paymentsystem/PaymentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
